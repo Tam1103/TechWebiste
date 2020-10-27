71403e9a95ab3168eb7f764ac003d3dd8d414e33
Tam Minh <minhtam110397@gmail.com>
2020-10-27 08:25:18 +0700

    add admin template complete

---
+++ TechWebsite/Areas/Admin/Controllers/DashBoardController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TechWebsite.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    [Route("admin/dashboard")]
+
+    public class DashBoardController : Controller
+    {
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
