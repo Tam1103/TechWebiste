1ad9f99422c53b6f60f861e3d952331ee542ee00
Tam Minh <minhtam110397@gmail.com>
2020-11-02 18:36:42 +0700

    perform add, edit, delete category in admin

---
+++ TechWebsite/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechWebsite.Models;
+
+namespace TechWebsite.Areas.Admin.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    [Area("admin")]
+    [Route("admin/category")]
+    public class CategoryController : Controller
+    {
+        private DatabaseContext db = new DatabaseContext();
+        public CategoryController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            ViewBag.categories = db.Categories.Where(c => c.Parent == null).ToList();
+            return View();
+        }
+
+        [Route("add")]
+        [HttpGet]
+        public IActionResult Add()
+        {
+            var category = new Category();
+            return View("Add", category);
+        }
+
+
+        [Route("add")]
+        [HttpPost]
+        public IActionResult Add(Category category)
+        {
+            category.Parent = null;
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return RedirectToAction("index", "category", new { area = "admin" });
+        }
+
+        [HttpGet]
+        [Route("delete/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var categories = db.Categories.Find(id);
+            db.Categories.Remove(categories);
+            db.SaveChanges();
+            return RedirectToAction("index", "category", new { area = "admin" });
+        }
+
+        [HttpGet]
+        [Route("edit/{id}")]
+        public IActionResult Edit(int id)
+        {
+            var categories = db.Categories.Find(id);
+            return View("Edit", categories);
+        }
+
+        [HttpPost]
+        [Route("edit/{id}")]
+        public IActionResult Edit(int id,Category category)
+        {
+            var currentCategory = db.Categories.Find(id);
+            category.Name = category.Name;
+            category.Status = category.Status;
+            db.SaveChanges();
+            return RedirectToAction("index", "category", new { area = "admin" });
+        }
+    }
+}
--- TechWebsite/Models/DatabaseContext.cs
+++ TechWebsite/Models/DatabaseContext.cs
@@ -23,9 +23,40 @@ namespace TechWebsite.Models
 
         public virtual DbSet<RoleAccount> RoleAccounts { get; set; }
 
+        public virtual DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.4-servicing-10062");
+
+            modelBuilder.Entity<Account>(entity =>
+            {
+                entity.Property(e => e.Email)
+                 .HasMaxLength(250)
+                 .IsUnicode(false);
+
+                entity.Property(e => e.FullName)
+                  .HasMaxLength(50)
+                  .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                  .HasMaxLength(250)
+                  .IsUnicode(false);
+
+                entity.Property(e => e.Username)
+                  .HasMaxLength(250)
+                  .IsUnicode(false);
+            });
+
+
+            modelBuilder.Entity<Role>(entity =>
+            {
+                entity.Property(e => e.Name)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+
+            });
+
             modelBuilder.Entity<RoleAccount>(entity =>
             {
                 entity.HasKey(e => new { e.RoleId, e.AccountId });
@@ -34,14 +65,26 @@ namespace TechWebsite.Models
                 .WithMany(p => p.RoleAccounts)
                 .HasForeignKey(d => d.AccountId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Account_Role_Account");
+                .HasConstraintName("FK_RoleAccount_Account");
 
 
                 entity.HasOne(d => d.Role)
                 .WithMany(p => p.RoleAccounts)
                 .HasForeignKey(d => d.RoleId)
                 .OnDelete(DeleteBehavior.ClientSetNull)
-                .HasConstraintName("FK_Account_Role_Role");
+                .HasConstraintName("FK_RoleAccount_Role");
+            });
+
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.Name)
+                .HasMaxLength(250)
+                .IsUnicode(false);
+                
+                entity.HasOne(d => d.Parent)
+                .WithMany(p => p.InverseParents)
+                .HasForeignKey(d => d.ParentId)
+                .HasConstraintName("FK_Category_Category");
             });
         }
     }
