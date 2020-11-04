912fc41d88ae4b244934db62aaabd98d66c0f2b2
Tam Minh <minhtam110397@gmail.com>
2020-11-04 18:02:51 +0700

    complete dis category in user

---
+++ TechWebsite/ViewComponents/SearchViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechWebsite.Models;
+
+namespace TechWebsite.ViewComponents
+{
+    [ViewComponent(Name = "Search")]
+    public class SearchViewComponent : ViewComponent
+    {
+        private DatabaseContext db;
+        public SearchViewComponent(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            List<Category> categories = db.Categories.Where(c => c.Parent == null && c.Status).ToList();
+            return View("Index",categories);
+        } 
+    }
+}
