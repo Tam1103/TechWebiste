4127bcb193d22a10482a757c9ae6293612416e52
Tam Minh <minhtam110397@gmail.com>
2020-11-05 12:17:53 +0700

    add controller product

---
+++ TechWebsite/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TechWebsite.Models;
+
+namespace TechWebsite.Areas.Admin.Controllers
+{
+
+    [Authorize(Roles = "Admin")]
+    [Area("admin")]
+    [Route("admin/product")]
+    public class ProductController : Controller
+    {
+
+        private readonly DatabaseContext db;
+        public ProductController()
+        {
+        }
+        public ProductController(DatabaseContext _db)
+        {
+            db = _db;
+        }
+
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
