b8dac9e27fa173c0ef6fc8c4ecd782868b85836a
Tam Minh <minhtam110397@gmail.com>
2020-11-05 20:01:55 +0700

    complete add product in admin

--- TechWebsite/Areas/Admin/Controllers/ProductController.cs
+++ TechWebsite/Areas/Admin/Controllers/ProductController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using TechWebsite.Areas.Admin.Models.ViewModels;
 using TechWebsite.Models;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TechWebsite.Areas.Admin.Controllers
 {
@@ -26,7 +28,46 @@ namespace TechWebsite.Areas.Admin.Controllers
         public IActionResult Index()
         {
             var product = db.Products.ToList();
-            return View("index",product);
+            return View("index", product);
+        }
+
+        [HttpGet]
+        [Route("add")]
+        public IActionResult Add()
+        {
+            var productViewModel = new ProductViewModel();
+            productViewModel.Product = new Product();
+            productViewModel.Categories = new List<SelectListItem>();
+
+            var categories = db.Categories.ToList();
+            foreach (var category in categories)
+            {
+                var group = new SelectListGroup { Name = category.Name };
+                if (category.InverseParents != null && category.InverseParents.Count() != 0)
+                {
+                    foreach (var subCategory in category.InverseParents)
+                    {
+                        var selectListItem = new SelectListItem
+                        {
+                            Text = subCategory.Name,
+                            Value = subCategory.Id.ToString(),
+                            Group = group
+                        };
+
+                        productViewModel.Categories.Add(selectListItem);
+                    }
+                }
+            }
+            return View("Add", productViewModel);
+        }
+
+        [HttpPost]
+        [Route("add")]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            db.Products.Add(productViewModel.Product);
+            db.SaveChanges();
+            return RedirectToAction("index", "product", new { area = "admin" });
         }
     }
 }
